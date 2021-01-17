602f6cf8550c0f340af084d41645ccb8bab968b1
barkatme <alexander.butgusaim@gmail.com>
2021-01-17 14:52:37 +0300

    fix migrations path

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ flyway {
 repositories {
     mavenLocal()
     jcenter()
-    maven { url = uri("https://jitpack.io") }
     maven { url = uri("https://kotlin.bintray.com/ktor") }
     maven { url = uri("https://kotlin.bintray.com/kotlin-js-wrappers") }
 }
